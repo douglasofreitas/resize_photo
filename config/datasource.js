ca44efe31bd81464b86f11df4ead972f2e8f83bc
Douglas Freitas <douglasfreitas@natura.net>
2017-08-03 22:48:38 -0300

    docker v2

--- config/datasource.js
+++ config/datasource.js
@@ -3,9 +3,44 @@ import Promise from 'bluebird';
 import config from './config';
 import ImageSchema from '../src/models/Image';
 
-const options = { promiseLibrary: Promise };
+const options = {
+  promiseLibrary: Promise,
+  server: { auto_reconnect: true, socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+};
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://${config.database_host}/${config.database}`, options);
 mongoose.model('Image', ImageSchema);
+
+function connectDatabase() {
+  mongoose.connect(`mongodb://${config.database_host}/${config.database}`, options);
+}
+connectDatabase();
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose default connection open to ');
+});
+
+mongoose.connection.on('reconnected', () => {
+  console.log('MongoDB reconnected!');
+});
+// If the connection throws an error
+mongoose.connection.on('error', () => {
+  console.log('Mongoose default connection error: ');
+  setTimeout(() => { connectDatabase(); }, 1000);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
 
 export default() => mongoose;
